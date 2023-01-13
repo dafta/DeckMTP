1550b2a4735a678d7fb043009011daa8604e3bff
Karlo Dautović <karlo.dautovic@infigo.hr>
2023-01-13 15:27:14 +0100

    Added ability to toggle MTP
    Also added a message when the user doesn't have DRD enabled, with some
    instructions on how to enable it.

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,18 @@ import {
   staticClasses,
 } from "decky-frontend-lib";
 import { useState, useEffect, VFC } from "react";
-import { FaShip } from "react-icons/fa";
+import { FaFolder } from "react-icons/fa";
 
 import * as backend from "./backend";
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
   const [mtpEnabled, setMtpEnabled] = useState<boolean>(false);
+  const [drdEnabled, setDrdEnabled] = useState<boolean>(false);
 
   useEffect(() => {
+    backend.is_drd_enabled().then((enabled) => {
+      setDrdEnabled(enabled);
+    });
     backend.is_running().then((running) => {
       setMtpEnabled(running);
     });
@@ -26,8 +30,24 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
         <ToggleField
           label="Enable MTP"
           checked={mtpEnabled}
+          disabled={!drdEnabled}
+          onChange={async () => {
+            await backend.toggle_mtp()
+          }}
         />
       </PanelSectionRow>
+      {!drdEnabled && (
+        <div>
+          <strong>
+            <em>DRD is disabled</em>
+          </strong>
+          <br/>
+          In order for MTP file transfer to work correctly, DRD
+          (Dual-Role Device) must be enabled in the BIOS settings,
+          under <em>Advanced</em>, <em>USB Configuration</em>,
+          <em>USB Dual-Role Device</em>.
+        </div>
+      )}
     </PanelSection>
   );
 };
@@ -38,8 +58,9 @@ export default definePlugin((serverApi: ServerAPI) => {
   return {
     title: <div className={staticClasses.Title}>MTP</div>,
     content: <Content serverAPI={serverApi} />,
-    icon: <FaShip />,
+    icon: <FaFolder />,
     onDismount() {
+      backend.stop_mtp()
     },
   };
 });
