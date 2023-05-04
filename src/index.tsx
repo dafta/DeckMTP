7d0fdd1f4e364c08d0ee31fe97143b0b68c071db
Karlo Dautović <dafta007@gmail.com>
2023-05-04 19:17:22 +0200

    Add warning message in DRD instructions

--- src/index.tsx
+++ src/index.tsx
@@ -46,6 +46,13 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
           (Dual-Role Device) must be enabled in the BIOS settings,
           under <em>Advanced</em>, <em>USB Configuration</em>,
           <em>USB Dual-Role Device</em>.
+          <br/>
+          <strong>
+            <em>WARNING: USB doesn't work under Windows if DRD is
+            enabled. Additionally, booting from USB might not work.
+            Disabling DRD will solve these issues, if needed.
+            </em>
+          </strong>
         </div>
       )}
     </PanelSection>
