a6447d9f29b61084fa8f7adb1b86be5fc8372197
dafta <dafta007@gmail.com>
2024-06-22 00:57:48 +0200

    Replace existing MTP logic with systemd services

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-import pathlib
+from pathlib import Path
 import subprocess
 import sys
+
+from string import Template
 
 import decky_plugin
 
 
 # append py_modules to PYTHONPATH
-sys.path.append(str(pathlib.Path().cwd() / "py_modules"))
+sys.path.append(str(Path().cwd() / "py_modules"))
 
+# Constants to important folders
 PLUGIN_BIN_DIR: str = decky_plugin.DECKY_PLUGIN_DIR + "/bin"
+PLUGIN_SERVICES_DIR: str = PLUGIN_BIN_DIR + "/services"
+PLUGIN_SCRIPTS_DIR: str = PLUGIN_BIN_DIR + "/scripts"
+PLUGIN_CONFIGS_DIR: str = PLUGIN_BIN_DIR + "/configs"
+
+# Services to install
+SERVICES: list[str] = [
+    "gadget-init.service",
+    "gadget-start.service",
+    "umtprd.service",
+    "usbgadget-func-mtp.service",
+]
+
+# Configs to install, and the folders to install them into
+CONFIGS: list[tuple[str,str]] = [
+    ("gadget", decky_plugin.DECKY_PLUGIN_SETTINGS_DIR),
+    ("umtprd.conf", "/etc/umtprd"),
+]
 
 
 # Helper for systemctl commands
@@ -26,12 +46,45 @@ def is_running() -> bool:
 class Plugin:
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
-        pass
+        # Copy configs to correct directory
+        for config in CONFIGS:
+            input_file = Path(PLUGIN_CONFIGS_DIR, config[0])
+            output_folder = Path(config[1])
+            output_file = output_folder / config[0]
+
+            if not output_folder.exists():
+                output_folder.mkdir()
+
+            if not output_file.exists():
+                _ = output_file.write_text(input_file.read_text(encoding="utf-8"))
+
+        # Copy services to correct directory
+        for service in SERVICES:
+            input_file = Path(PLUGIN_SERVICES_DIR, service)
+
+            # Replace templates in services text
+            template = Template(input_file.read_text(encoding="utf-8"))
+            substitutions: dict[str, str] = {
+                "bindir": PLUGIN_BIN_DIR,
+                "scriptsdir": PLUGIN_SCRIPTS_DIR,
+                "envfile": decky_plugin.DECKY_PLUGIN_SETTINGS_DIR + "/gadget",
+            }
+
+            # Write substituted service
+            output_file = Path(decky_plugin.DECKY_PLUGIN_RUNTIME_DIR, service)
+            _ = output_file.write_text(
+                template.safe_substitute(substitutions), encoding="utf-8"
+            )
+
+        # Link services to systemd
+        for service in SERVICES:
+            service_file = Path(decky_plugin.DECKY_PLUGIN_RUNTIME_DIR, service)
+            _ = systemctl("link", str(service_file))
 
     # Function called first during the unload process,
     # utilize this to handle your plugin being removed
     async def _unload(self):
-        await self.stop_mtp()
+        _ = await Plugin.stop_mtp(self)
 
     # Check if umtprd is running
     async def is_running(self) -> bool:
@@ -49,22 +102,18 @@ class Plugin:
             return False
         return bool(result)
 
+    # Start MTP
+    async def start_mtp(self) -> bool:
+        return systemctl("start", "usb-gadget.target")
+
+    # Stop MTP
+    async def stop_mtp(self) -> bool:
+        return systemctl("stop", "gadget-init.service")
+
     # Toggle MTP
     async def toggle_mtp(self) -> bool:
         if not is_running():
-            _ = subprocess.run(
-                "./scripts/start.sh", cwd=PLUGIN_BIN_DIR, check=False, shell=True
-            )
+            _ = Plugin.start_mtp(self)
         else:
-            _ = subprocess.run(
-                "./scripts/stop.sh", cwd=PLUGIN_BIN_DIR, check=False, shell=True
-            )
+            _ = Plugin.stop_mtp(self)
         return is_running()
-
-    # Stop MTP
-    async def stop_mtp(self):
-        if not is_running():
-            return
-        _ = subprocess.run(
-            "./scripts/stop.sh", cwd=PLUGIN_BIN_DIR, check=False, shell=True
-        )
